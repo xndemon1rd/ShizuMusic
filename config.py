7ad34cd6a97b02362f5ce8611acd3025be6c8cb7
xndemon1rd <xndemon1@gmail.com>
2026-09-09 19:07:10 +0530

    Update bot link and support group in config.py

--- config.py
+++ config.py
@@ -17,9 +17,9 @@ OWNER_ID        = int(os.environ["7983098956"])
 
 # ── Optional ──────────────────────────────────────────────────────────────────
 BOT_NAME         = os.getenv("BOT_NAME", "Shizu Music")
-BOT_LINK         = os.getenv("BOT_LINK", "https://t.me/ShizuMusicBot")
+BOT_LINK         = os.getenv("BOT_LINK", "https://t.me/yormusicxbot")
 UPDATES_CHANNEL  = os.getenv("UPDATES_CHANNEL", "https://t.me/PBX_UPDATE")
-SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "https://t.me/PBXCHATS")
+SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "https://t.me/zpaveldurov")
 LOGGER_ID        = int(os.getenv("LOGGER_ID", "0"))
 PING_IMG_URL     = os.getenv("PING_IMG_URL", "https://files.catbox.moe/ddzvc0.jpg",)
 SESSION_NAME     = os.getenv("SESSION_NAME", "ShizuMusic")
