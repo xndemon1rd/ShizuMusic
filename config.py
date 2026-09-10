a99bc5473baeba8e0ed3e3cea4c4671c17112681
xndemon1rd <xndemon1@gmail.com>
2026-09-10 15:19:19 +0530

    Update API_ID and API_HASH in config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ── Required ──────────────────────────────────────────────────────────────────
-API_ID          = int(os.environ["30804214"])
-API_HASH        = os.environ["868c14bb613d143a45d1eaa5a17d8e1b"]
+API_ID          = int(os.environ["34944026"])
+API_HASH        = os.environ["d594eb23fbadca610d271dec57d7833b"]
 BOT_TOKEN       = os.environ["8803348450:AAGmrTYUbkTl7fXVAnPz-MLZ1VX16igPoak"]
 STRING_SESSION  = os.environ["STRING_SESSION"]
 MONGO_DB_URL    = os.environ["mongodb+srv://la045514_db_user:Zm40Moe01trMFw71@cluster1.0r0xacx.mongodb.net/?appName=Cluster1"]
