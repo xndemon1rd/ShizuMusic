95d47ca604551fe461ca84d9bdcc245e38df2286
xndemon1rd <xndemon1@gmail.com>
2026-09-10 16:39:55 +0530

    Update MongoDB connection string format

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID          = int(os.environ["34944026"])
 API_HASH        = os.environ["d594eb23fbadca610d271dec57d7833b"]
 BOT_TOKEN       = os.environ["8803348450:AAGmrTYUbkTl7fXVAnPz-MLZ1VX16igPoak"]
 STRING_SESSION  = os.environ["STRING_SESSION"]
-MONGO_DB_URL    = os.environ["mongodb://atlas-sql-6aa2886e46168823e3a7fa4b-xpufmy.g.query.mongodb.net/sample_mflix?ssl=true&authSource=admin"]
+MONGO_DB_URL    = os.environ["mongodb+srv://<db_username>:5CHj3CJiJhrFqvXv@cluster0.iebzzx9.mongodb.net/?appName=Cluster0"]
 OWNER_ID        = int(os.environ["8076712430"])
 
 # ── Optional ──────────────────────────────────────────────────────────────────
